1c0c5b3ae4de6413f37e913006c4a5828863badd
jprx <36464332+jprx@users.noreply.github.com>
2026-03-20 10:13:40 -0500

    Update al68 fork to sync with upstream

--- keyboards/yunzii/al68/al68.c
+++ keyboards/yunzii/al68/al68.c
@@ -1,11 +1,11 @@
 // Modified from https://github.com/yunziikeyboard/qmk_firmware
 #include "quantum.h"
+#include "uart.h"
 
 void keyboard_pre_init_kb(void) {
-    // without this, the K key and a few others won't work:
     AFIO->MAPR = (AFIO->MAPR & ~AFIO_MAPR_SWJ_CFG_Msk);
     AFIO->MAPR |= AFIO_MAPR_SWJ_CFG_DISABLE;
 
-    // without this, no keys work:
     gpio_set_pin_output(A8);
+    gpio_write_pin_high(A8);
 }
